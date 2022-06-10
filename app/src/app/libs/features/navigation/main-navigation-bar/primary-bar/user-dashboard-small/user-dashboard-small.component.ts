1484e607dc3c08d6d99011536c279ff22de692f1
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-06-10 16:24:24 +0200

    feat: hides nav bar user thingy

--- app/src/app/libs/features/navigation/main-navigation-bar/primary-bar/user-dashboard-small/user-dashboard-small.component.ts
+++ app/src/app/libs/features/navigation/main-navigation-bar/primary-bar/user-dashboard-small/user-dashboard-small.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { ShowAuthentication } from "src/app/libs/features/authentication/store/actions";
 
@@ -9,9 +9,17 @@ import { ShowAuthentication } from "src/app/libs/features/authentication/store/a
   styleUrls: ['./user-dashboard-small.component.scss'],
 })
 export class UserDashboardSmallComponent {
+
+  @Output()
+  clicked = new EventEmitter<boolean>();
+
   constructor(
     private readonly store: Store
   ) {}
+
+  onClick() {
+    this.clicked.emit(true);
+  }
 
   showAuthentication() {
     this.store.dispatch(ShowAuthentication({ showAuthentication: true}));
